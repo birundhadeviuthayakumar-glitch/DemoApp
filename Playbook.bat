555fe6b204353075e237847521a699014352b0ac
birundhadeviuthayakumar-glitch <birundhadeviuthayakumar@gmail.com>
2026-05-06 10:38:40 +0530

    Add files via upload

---
+++ Playbook.bat
@@ -0,0 +1,19 @@
+@echo off
+echo ==========================
+echo Starting Ansible
+echo ==========================
+echo.
+echo Building Application
+javac App.java
+echo.
+echo Testing the application
+echo Testing completed
+echo.
+echo Deploying Application
+java App
+echo Deployment processing
+echo.
+echo ==========================
+echo Deployment Success
+echo ==========================
+pause
